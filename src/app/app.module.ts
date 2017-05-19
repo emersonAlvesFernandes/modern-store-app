ea3a2085e4c6fde67271fc229b922bc6bb77b5f6
emersonAlvesFernandes <emersonej1@redspark.io>
2017-05-19 19:42:41 -0300

    telas + rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+// Rotas 
+import{Routing, RoutingProviders} from './app.routing';
+
+
+//  Root
 import { AppComponent } from './app.component';
+
+//  Shared
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+//  Components
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+//  Pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadbarComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignUpPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//  Pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+
+const appRoutes: Routes= [
+    { path: '', component: LoginPageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'signup', component: SignUpPageComponent },
+    { path: 'cart', component: CartPageComponent },
+];
+
+export const RoutingProviders: any[] = [];
+export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
